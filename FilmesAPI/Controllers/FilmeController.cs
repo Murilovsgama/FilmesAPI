49d15e863972ab10cbf917e33b0fa4df6c7f9177
Murilo <Murilo@DESKTOP-RC0E3BN>
2024-08-26 21:30:34 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmesAPI.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")] // Nomeando assim, basicamente ele vai cortar a controller do nome e manter apenas como Filme
+                      // , assim caso eu renomeie essa classe, não vou precisar mudar o nome da rota
+public class FilmeController :ControllerBase
+{
+
+    private static List <Filme> filmes =  new List<Filme> ();
+    private static int id = 1;
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+
+        filme.ID = id++;
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperarPorID(int id)
+    {
+      return  filmes.FirstOrDefault(filme => filme.ID == id);
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int ID { get; set; }
+    [Required(ErrorMessage ="Titulo do filme é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O genero deve conter no máximo 100 caracteres")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "Genero do filme é obrigatório.")]
+    [MaxLength(100, ErrorMessage ="O genero deve conter no máximo 100 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "Duracao do filme é obrigatório.")]
+    [Range(60 , 600, ErrorMessage ="A duração do filme deve ser entre 60 e 600 minutos")]
+    public int Duracao { get; set; }
+}
